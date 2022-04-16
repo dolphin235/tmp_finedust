3e0cdf44a6ab7685563b991b5cb5789b0f4fffc3
dolphin235 <dolphin222@daum.net>
2022-04-16 15:58:05 +0900

    API xml parsing practice

---
+++ java/ApiExplorer.java
@@ -0,0 +1,142 @@
+package parsing;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class ApiExplorer {
+    public static void main(String[] args) throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=kj9rVtwid%2FEpOFekRhn%2Bzyu2KBcIReuxj%2BpLdZ1NuMyi1Im5XORdmUe9lkxXAAHBNqEhOg18ATJOj4Lqs5MM%2FA%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8")); /*xml 또는 json*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("100", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("sidoName","UTF-8") + "=" + URLEncoder.encode("서울", "UTF-8")); /*시도 이름(전국, 서울, 부산, 대구, 인천, 광주, 대전, 울산, 경기, 강원, 충북, 충남, 전북, 전남, 경북, 경남, 제주, 세종)*/
+        urlBuilder.append("&" + URLEncoder.encode("ver","UTF-8") + "=" + URLEncoder.encode("1.0", "UTF-8")); /*버전별 상세 결과 참고*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        //System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line+"\n");
+        }
+        rd.close();
+        conn.disconnect();
+        //System.out.println(sb.toString());
+        xml_parsing(sb.toString(), "종로구");
+    }
+    
+    public static void xml_parsing(String data, String target_location) {
+    	try {
+	    	String xml = data;
+	    	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    	DocumentBuilder builder = factory.newDocumentBuilder();
+	    	
+	    	InputStream instream = new ByteArrayInputStream(xml.getBytes());
+	    	Document doc = builder.parse(instream);
+	    	Element root = doc.getDocumentElement();
+	    	NodeList items = root.getElementsByTagName("item");
+	    	
+	    	for(int i=0;i<items.getLength();i++) {
+	    		Node item = items.item(i);
+	    		NodeList nodeList = item.getChildNodes();
+	    		
+	    		String tmp_location = "none";
+	    		String tmp_finedust = "none";
+	    		String tmp_vfinedust = "none";
+	    		
+	    		for(int j=0;j<nodeList.getLength();j++) {
+	    			Node cur_node = nodeList.item(j);
+	    			if (cur_node.getNodeName().equals("stationName")) {
+	    				tmp_location = cur_node.getTextContent();
+	    			}
+	    			if (cur_node.getNodeName().equals("pm10Value")) {
+	    				tmp_finedust = cur_node.getTextContent();
+	    			}
+	    			if (cur_node.getNodeName().equals("pm25Value")) {
+	    				tmp_vfinedust = cur_node.getTextContent();
+	    			}
+	    		}
+  
+    			if(tmp_location.equals(target_location)) {
+    				System.out.println("지역  : "+tmp_location);
+        			System.out.println("미세  : "+tmp_finedust);
+        			System.out.println("초미세 : "+tmp_vfinedust);
+        			
+        			System.out.println("미세 등급: "+finedust_level(tmp_finedust));
+        			System.out.println("초미세 등급: "+vfinedust_level(tmp_vfinedust));
+        			
+    				break;
+    			}
+    			
+	    	}
+    	}
+    	catch(Exception e) {
+    		System.out.println(e.toString());
+    	}
+	    	
+    }
+    
+    public static String finedust_level(String input) {
+    	if (input.equals('-')) {
+    		return input;
+    	}
+    	int value = Integer.parseInt(input);
+    	
+    	if (value <= 30) {
+    		return "좋음";
+    	}
+    	else if (value <= 80) {
+    		return "보통";
+    	}
+    	else if (value <= 150) {
+    		return "나쁨";
+    	}
+    	else {
+    		return "매우나쁨";
+    	}
+    }
+    
+    public static String vfinedust_level(String input) {
+    	if (input.equals('-')) {
+    		return input;
+    	}
+    	int value = Integer.parseInt(input);
+    	
+    	if (value <= 15) {
+    		return "좋음";
+    	}
+    	else if (value <= 35) {
+    		return "보통";
+    	}
+    	else if (value <= 75) {
+    		return "나쁨";
+    	}
+    	else {
+    		return "매우나쁨";
+    	}
+    }
+}
--- java/par.java
+++ java/par.java
@@ -16,27 +16,46 @@ public class par {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document document = builder.parse("./test.xml");
-			
-			System.out.println("홍길동");
-			return;
-			
-			/*
+
 			Element root = document.getDocumentElement(); // sample
 			System.out.println("[1]"+root.getNodeName());
 			
 			Node firstNode = root.getFirstChild();
 			System.out.println("[2]"+firstNode.getNodeName());
 			
-			Node secondNode = firstNode.getNextSibling();
-			System.out.println("[3]"+secondNode.getNodeName());
-			
-			NodeList childList = secondNode.getChildNodes();
-			for(int i=0;i<childList.getLength();i++) {
-				Node item = childList.item(i);
-				System.out.println(item.getNodeName());
-				System.out.println(item.getTextContent());
+			NodeList secondNodes = root.getChildNodes();
+			for (int i=0;i<secondNodes.getLength();i++) {
+				System.out.println("[3]"+secondNodes.item(i).getNodeName());
+				if(secondNodes.item(i).getNodeName().equals("body")) {
+					Node body_node = secondNodes.item(i);
+					Node items_node = body_node.getFirstChild();
+					Node second_node = items_node.getNextSibling();
+					Node item_node = second_node.getFirstChild();
+					Node second_item_node = item_node.getNextSibling();
+					//System.out.println("[5]"+second_item_node.getNodeName());
+					NodeList itemList = second_item_node.getChildNodes();
+					for (int j=0;j<itemList.getLength();j++) {
+						Node tmp_item = itemList.item(j);
+//						System.out.println(tmp_item.getNodeName());
+//						System.out.println(tmp_item.getTextContent());
+						
+						if(tmp_item.getNodeName().equals("stationName")) {
+							System.out.println(tmp_item.getTextContent());
+						}
+					}
+				}
 			}
-			*/
+			
+//			NodeList childList = secondNode.getChildNodes();
+//			for(int i=0;i<childList.getLength();i++) {
+//				Node item = childList.item(i);
+//				System.out.println(item.getNodeName());
+//				System.out.println(item.getTextContent());
+//				
+//				if(item.getNodeName().equals("name")) {
+//					System.out.println(item.getTextContent());
+//				}
+//			}
 			
 		} catch (Exception e) {
 			System.out.println(e);
